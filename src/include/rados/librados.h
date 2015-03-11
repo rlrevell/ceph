af2b530747d7c82154de83df5bbda71eef810e3b
Andrew Shewmaker <agshew@gmail.com>
2015-03-10 19:43:29 -0700

    Rados support for blkin (LTTng + Zipkin) tracing
     * Adds traced versions of Rados aio_read/write
    
    Signed-off-by: Marios-Evaggelos Kogias <marioskogias@gmail.com>
    Signed-off-by: Filippos Giannakos <philipgian@grnet.gr>
    Signed-off-by: Andrew Shewmaker <agshew@gmail.com>
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- src/include/rados/librados.h
+++ src/include/rados/librados.h
@@ -1736,6 +1736,13 @@ CEPH_RADOS_API int rados_aio_write(rados_ioctx_t io, const char *oid,
 		                   rados_completion_t completion,
 		                   const char *buf, size_t len, uint64_t off);
 
+#ifdef WITH_BLKIN
+struct blkin_trace_info;
+int rados_aio_write_traced(rados_ioctx_t io, const char *oid,
+        rados_completion_t completion,
+        const char *buf, size_t len, uint64_t off,
+        struct blkin_trace_info *info);
+#endif
 /**
  * Asychronously append data to an object
  *
@@ -1818,6 +1825,12 @@ CEPH_RADOS_API int rados_aio_read(rados_ioctx_t io, const char *oid,
 		                  rados_completion_t completion,
 		                  char *buf, size_t len, uint64_t off);
 
+#ifdef WTIH_BLKIN
+int rados_aio_read_traced(rados_ioctx_t io, const char *oid,
+        rados_completion_t completion,
+        char *buf, size_t len, uint64_t off,
+        struct blkin_trace_info *info);
+#endif
 /**
  * Block until all pending writes in an io context are safe
  *
