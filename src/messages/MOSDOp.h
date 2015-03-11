c6e3b65156d1d2131ea9f9568e79af3a4e4aa85c
Andrew Shewmaker <agshew@gmail.com>
2015-03-10 19:43:28 -0700

    OSD support for blkin (LTTng + Zipkin) tracing
     * Adds tracing for OSDs and Placement Groups, specifically:
       messages for OSD operations, FileJournal, FileStore,
       ECBackend, replication, and traced versions of Objecter
       read/write (only basic read/write functions).
     * Moves global_init_daemonize() earlier in ceph_osd.c:main
    
    Signed-off-by: Marios-Evaggelos Kogias <marioskogias@gmail.com>
    Signed-off-by: Filippos Giannakos <philipgian@grnet.gr>
    Signed-off-by: Andrew Shewmaker <agshew@gmail.com>
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- src/messages/MOSDOp.h
+++ src/messages/MOSDOp.h
@@ -32,7 +32,11 @@ class OSD;
 
 class MOSDOp : public Message {
 
+#ifdef WITH_BLKIN
+  static const int HEAD_VERSION = 6;
+#else
   static const int HEAD_VERSION = 5;
+#endif
   static const int COMPAT_VERSION = 3;
 
 private:
@@ -185,6 +189,7 @@ public:
 
   // marshalling
   virtual void encode_payload(uint64_t features) {
+    BLKIN_GET_MASTER(mt);
 
     OSDOp::merge_osd_op_vector_in_data(ops, data);
 
@@ -261,11 +266,14 @@ struct ceph_osd_request_head {
 
       ::encode(retry_attempt, payload);
       ::encode(features, payload);
+
+      BLKIN_MSG_ENCODE_TRACE();
     }
   }
 
   virtual void decode_payload() {
     bufferlist::iterator p = payload.begin();
+    BLKIN_MSG_DO_INIT_TRACE();
 
     if (header.version < 2) {
       // old decode
@@ -348,6 +356,8 @@ struct ceph_osd_request_head {
 	::decode(features, p);
       else
 	features = 0;
+
+      BLKIN_MSG_DECODE_TRACE(6);
     }
 
     OSDOp::split_osd_op_vector_in_data(ops, data);
@@ -390,6 +400,9 @@ struct ceph_osd_request_head {
     out << " e" << osdmap_epoch;
     out << ")";
   }
+
+  BLKIN_MSG_INFO_DECL("MOSDOp")
+  BLKIN_MSG_END_DECL("MOSDOp")
 };
 
 
